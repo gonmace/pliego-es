dc87972eb31b431bfcd5a77f0613679bcaf79331
gonmace <magoreal38@gmail.com>
2025-05-19 20:31:02 -0400

    final

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Espera a que el contenedor de PostgreSQL esté disponible en el puerto 5432
-while ! nc -z chat-legal-db 5432; do
+while ! nc -z pliego-db 5432; do
   echo "Esperando a que PostgreSQL arranque..."
   sleep 1
 done
@@ -9,6 +9,6 @@ done
 echo "PostgreSQL está listo"
 
 # Ejecutar Gunicorn después de que PostgreSQL esté listo
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8002 --workers 3
 
 exec "$@"
